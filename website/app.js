73f6109fc62490a1ffc16d0c77658265cef5a6a1
Surya Jasper <suryajasper2004@gmail.com>
2020-06-14 23:20:45 -0700

    awoeijfowiaefj

--- website/app.js
+++ website/app.js
@@ -123,21 +123,21 @@ function sendToAI() {
 
   req.end(function(res) {
     var foodbanks = res.body;
-    warehouse.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').once('value', function(snapshot) {
+    warehouse.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').once('value', function(snapshot) {
       for (var bankName of Object.keys(foodbanks)) {
-        banks.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').child(bankName).once('value', function(bankSnapshot) {
+        banks.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').child(bankName).once('value', function(bankSnapshot) {
           for (var foodName of Object.keys(foodbanks[bankName])) {
-            warehouse.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').child(foodName).update({
+            warehouse.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').child(foodName).update({
               quantity: Math.min(snapshot.val()[bankName][foodName].quantity-foodbanks[bankName][foodName], 0)
             });
             if (foodName in bankSnapshot.val()) {
-              banks.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').child(bankName).child('foods').child(foodName).update({
+              banks.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').child(bankName).child('foods').child(foodName).update({
                 quantity: parseInt(bank.snapshot.val()[foodName].quantity) + foodbanks[bankName][foodName]
               })
             } else {
               var update = {};
               update[foodName] = snapshot.val()[foodName];
-              banks.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').child(bankName).child('foods').update(update);
+              banks.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').child(bankName).child('foods').update(update);
             }
           }
         });
@@ -147,7 +147,7 @@ function sendToAI() {
 }
 
 app.get('/warehouse', async function(req, res) {
-  warehouse.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').once('value', function(snapshot) {
+  warehouse.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').once('value', function(snapshot) {
     res.status(200);
   	res.json(Object.values(snapshot.val()));
   	res.end();
@@ -155,7 +155,7 @@ app.get('/warehouse', async function(req, res) {
 });
 
 app.get('/shelters', async function(req, res) {
-  banks.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').once('value', function(snapshot) {
+  banks.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').once('value', function(snapshot) {
     var homelessArr = [];
     for (var _bank of Object.keys(snapshot.val())) {
       if ('shelters' in snapshot.val()[_bank]) {
@@ -204,7 +204,7 @@ app.get('/preferences', async function (req, res) {
 })
 
 app.get('/banks', async function(req, res) {
-  banks.child('wQVmzq74oNMdTleSKiQW9TbbVWh2').once('value', function(snapshot) {
+  banks.child('lZJYEWkanBfbxeVY5qfdzMqtJTA3').once('value', function(snapshot) {
     res.status(200);
     var toReturn = {};
     var allBanks = snapshot.val();
